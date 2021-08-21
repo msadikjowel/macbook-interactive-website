1197c7000129498f74ecf55335248cfb6e484dfe
Mohammed Sadik <msadikjowel@gmail.com>
2021-08-21 15:40:24 +0600

    function added

--- js/app.js
+++ js/app.js
@@ -26,6 +26,7 @@ const deliveryCharge = document.getElementById('delivery-charge');
 
 // best price call
 const bestPrice = document.getElementById('best-price');
+
 // total price call
 const totalPrice = document.getElementById('total-price');
 
@@ -34,6 +35,7 @@ const grandTotal = document.getElementById('grand-total');
 
 // input field call
 const inputField = document.getElementById('input-text');
+
 // input button call
 const applyButton = document.getElementById('apply-button');
 
@@ -41,26 +43,43 @@ const applyButton = document.getElementById('apply-button');
     All Function 
  ---------------------*/
 
-// total function
+// total function - (bonus function - 1)
 function totalAmount() {
     const extraMemoryNumber = parseInt(extraMemory.innerText);
     const extraStorageNumber = parseInt(extraStorage.innerText);
     const deliveryChargeNumber = parseInt(deliveryCharge.innerText);
     const bestPriceNumber = parseInt(bestPrice.innerText);
+
     const totalPriceAmount = extraMemoryNumber + extraStorageNumber + deliveryChargeNumber + bestPriceNumber;
     totalPrice.innerText = totalPriceAmount;
     grandTotal.innerText = totalPriceAmount;
 }
 
+// selected button border- (bonus function - 2)
+function isContain(element) {
+    return element.classList.contains('selected');
+}
+
 // memory function
 memory8.addEventListener('click', function () {
     extraMemory.innerText = '0';
+    // bonus function-1
     totalAmount();
+    // bonus function-2
+    if (!isContain(memory8)) {
+        memory8.classList.add('selected');
+        memory16.classList.remove('selected');
+    }
 
 });
 memory16.addEventListener('click', function () {
     extraMemory.innerText = '180';
     totalAmount()
+    // bonus function-2
+    if (!isContain(memory16)) {
+        memory16.classList.add('selected');
+        memory8.classList.remove('selected');
+    }
 
 });
 
@@ -68,16 +87,32 @@ memory16.addEventListener('click', function () {
 storage256.addEventListener('click', function () {
     extraStorage.innerText = '0';
     totalAmount();
-
+    // bonus function-2
+    if (!isContain(storage256)) {
+        storage256.classList.add('selected');
+        storage512.classList.remove('selected');
+        storage1024.classList.remove('selected');
+    }
 });
 storage512.addEventListener('click', function () {
     extraStorage.innerText = '100';
     totalAmount();
-
+    // bonus function-2
+    if (!isContain(storage512)) {
+        storage512.classList.add('selected');
+        storage256.classList.remove('selected');
+        storage1024.classList.remove('selected');
+    }
 });
 storage1024.addEventListener('click', function () {
     extraStorage.innerText = '180';
     totalAmount();
+    // bonus function-2
+    if (!isContain(storage1024)) {
+        storage1024.classList.add('selected');
+        storage256.classList.remove('selected');
+        storage512.classList.remove('selected');
+    }
 
 });
 
@@ -85,11 +120,21 @@ storage1024.addEventListener('click', function () {
 deliveryFree.addEventListener('click', function () {
     deliveryCharge.innerText = '0';
     totalAmount();
+    // bonus function-2
+    if (!isContain(deliveryFree)) {
+        deliveryFree.classList.add('selected');
+        delivery20.classList.remove('selected');
+    }
 
 });
 delivery20.addEventListener('click', function () {
     deliveryCharge.innerText = '20';
     totalAmount();
+    // bonus function-2
+    if (!isContain(delivery20)) {
+        delivery20.classList.add('selected');
+        deliveryFree.classList.remove('selected');
+    }
 
 });
 
@@ -103,13 +148,14 @@ applyButton.addEventListener('click', function () {
     const promoCode = 'stevekaku';
     const grand = grandTotal.innerText;
     inputField.value = '';
+
     if (inputText == promoCode) {
         let grandDiscount = grand * .20; // discount, 20% = .20;
         grandNewAmount = grand - grandDiscount;
         grandTotal.innerText = grandNewAmount;
     }
     else {
-        alert('invalid promo code')
+        alert('invalid promo code');
     }
-})
+});
 
